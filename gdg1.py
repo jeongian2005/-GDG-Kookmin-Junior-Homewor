81dfac16eb10bee83dedf1ad144451430cb3ffb3
jeongian2005 <jeongian2005@gmail.com>
2024-10-04 21:09:14 +0900

    Add files via upload

---
+++ gdg1.py
@@ -0,0 +1,9 @@
+def generate_hello_world_str():
+    return "Hello World 정이안"
+
+def print_console():
+    hello_str = generate_hello_world_str()
+    print(hello_str)
+
+if __name__ == "__main__":
+    print_console()
